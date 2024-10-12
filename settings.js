0bce05f8b7fecd77bc3f686ae00b4e58f33c57cb
wily <103911498+botreaction@users.noreply.github.com>
2024-10-12 13:31:46 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,6 @@
+const readStatusSpeed = 1000; // 1 detik (1000 milidetik)
+const autoReadStory = true; // Aktifkan auto read story secara default
+const bioActive = false; // Aktifkan auto bio secara default
+const autoTyping = false; // Nonaktifkan auto typing secara default
+
+module.exports = { readStatusSpeed, bioActive, autoReadStory, autoTyping };
